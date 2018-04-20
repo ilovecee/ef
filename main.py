2d7b15faf5307d65f561fee91c4f167c6ec3b123
Xu <xuchang@agylytyx.com>
2018-04-19 22:17:05 -0700

    first_commit

---
+++ main.py
@@ -0,0 +1,369 @@
+from flask import Flask, request, jsonify, render_template, flash, redirect, url_for, session, logging, send_file
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+import optimizer as opt
+from functools import wraps
+import datetime
+from azure.storage.file import FileService
+import os
+import os.path
+
+
+app = Flask(__name__)
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Mark1204'
+app.config['MYSQL_DB'] = 'efficientfrontier'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+# Initialize MySQL
+mysql = MySQL(app)
+
+@app.route('/')
+def index():
+  return render_template('home.html')
+
+
+
+
+
+# Check if user logged in
+
+def is_logged_in(f):
+  @wraps(f)
+  def wrap(*args, **kwargs):
+    if 'logged_in' in session:
+      return f(*args, **kwargs)
+    else:
+      flash('Unauthorized, Please login', 'danger')
+      return redirect(url_for('login'))  
+
+  return wrap
+
+
+# Register form class
+class RegisterForm(Form):
+  name = StringField('Name', [validators.Length(min = 1, max = 50)])
+  company = StringField ('Company', [validators.Length(max = 50)])
+  email = StringField('Email', [validators.Length(min = 6, max = 50)])
+  password = PasswordField('Password', [
+    validators.DataRequired(),
+    validators.EqualTo('confirm', message = 'Password do not match')
+  ])
+  confirm = PasswordField('Confirm Password')
+
+
+# Download Template File
+
+@app.route('/tempfile_download')
+@is_logged_in
+def tempfile_download():
+  return send_file('ef_template.xlsx', attachment_filename = 'ef_template.xlsx')
+
+
+
+
+
+# Register
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+  form = RegisterForm(request.form)
+  if request.method == 'POST' and form.validate():
+    name = form.name.data
+    company = form.company.data
+    email = form.email.data
+    password = sha256_crypt.encrypt(str(form.password.data))
+
+    # Create Cursor
+    cur = mysql.connection.cursor()
+
+    # Execute
+    cur.execute('INSERT INTO users(name, company, email, password) VALUES(%s, %s, %s, %s)', (name, company, email, password))
+
+    # Commit to DB
+    mysql.connection.commit()
+
+    # Close connection
+    cur.close()
+
+    flash('You are now registered! Please log in', 'success')
+
+    return redirect(url_for('login'))
+
+  return render_template('register.html', form=form)  
+
+
+
+
+
+# Login Page
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+  if request.method == 'POST':
+    # Get Form Fields
+    name = request.form['name']
+    password_candidate = request.form['password']
+
+    # Create Cursor
+    cur = mysql.connection.cursor()
+
+    # Get user by name
+    result = cur.execute ('SELECT * FROM users WHERE name = %s', [name])
+
+    if result > 0:
+      # Get stored hash
+      data = cur.fetchone()
+      password = data['password']
+
+      # Compare Passwords
+      if sha256_crypt.verify(password_candidate, password):
+        # Passed
+        session['logged_in'] = True
+        session['name'] = name
+
+        flash ('You are now logged in', 'success')
+        return redirect(url_for ('files'))
+      else:
+        error = 'Invalid Login'
+        return render_template('login.html', error = error)
+
+      # Close connection
+      cur.close()
+    else:
+      error = 'Username not found'
+      return render_template('login.html', error = error)
+
+  return render_template('login.html')  
+
+
+
+
+
+# Logout
+@app.route('/logout')
+@is_logged_in
+def logout():
+  session.clear()
+  flash('You are now logged out', 'success')
+  return redirect(url_for('login'))
+
+
+  
+
+
+# File dashboard
+@app.route('/files', methods = ["GET"])
+@is_logged_in
+def files():
+  # Create Cursor
+  cur = mysql.connection.cursor()
+
+  #Get articles
+  result = cur.execute('SELECT filename, USER, createdon FROM files_uploaded')
+
+  files = cur.fetchall()
+
+  if result > 0:
+    return render_template('files.html', files = files)
+
+  else:
+    msg = 'No files found, please upload your files first.'
+    return render_template('files.html', error = msg)
+
+  # Close connection
+  cur.close()
+
+
+@app.route('/files', methods = ["POST"])
+@is_logged_in
+def upload():
+  AzureStorageAccount = 'effiles'
+  key = 'axLykwdLsUwKTDY5flU6ivGrt9obV38k2UMVDCSpLYE3K6jAkwsjWOThQydhuMSWHfx6lTq102gdkas/GyKhEA=='
+  up_path = 'uploads'
+  path1 = 'efficientfrontier'
+  file_service = FileService(account_name = AzureStorageAccount, account_key = key)
+
+  target = os.path.join(APP_ROOT, 'uploads/')
+
+  if request.method == 'POST':
+    if not os.path.isdir(target):
+      os.mkdir(target)
+    
+
+ 
+    for file in request.files.getlist("file"):
+      filename = file.filename
+      destination = '/'.join([target, filename])
+      #print (destination)
+      file.save(destination)       # Save the file to local server
+      now = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+      filename_new = os.path.splitext(filename)[0] + '_' + now + os.path.splitext(filename)[1]
+      file_service.create_file_from_path (path1, up_path, filename_new, destination)    # Upload the file to Azure Storage Account
+        # Create Cursor
+      cur = mysql.connection.cursor()
+
+      # Execute
+      cur.execute ('INSERT INTO files_uploaded (filename, USER, AzureAccount, AzureShare, Directory) VALUES(%s, %s, %s, %s, %s)', (filename_new, session['name'], AzureStorageAccount, path1, up_path))
+
+      # Commit to DB
+      mysql.connection.commit()
+
+      # Close connection
+      cur.close()
+
+      flash ('File Uploaded', 'success')
+      os.remove(destination)  # Remove locally saved file
+    
+  return redirect(url_for('files'))
+
+
+
+
+@app.route('/optimizer/<string:filename>', methods = ['GET', 'POST'])
+@is_logged_in
+def optimize(filename):
+  data, AzureStorageAccount, path1, down_path, outfilename, key = opt.get_result(filename)
+  '''response = {'AzureStorageAccount': AzureStorageAccount,
+              'Folder1': path1,
+              'Folder2': down_path,
+              'Output File': outfilename,
+              'data': data,
+              'StorageAccountKey': key} 
+  res = jsonify(response)'''
+
+  # Create Cursor
+  cur = mysql.connection.cursor()
+
+  # Execute
+  result = cur.execute('SELECT filename FROM result_files WHERE filename = %s', [outfilename])
+  
+  if result == 0:
+    
+    cur.execute ('INSERT INTO result_files (filename, USER, AzureAccount, AzureShare, Directory) VALUES(%s, %s, %s, %s, %s)', (outfilename, session['name'], AzureStorageAccount, path1, down_path))
+
+
+  # Commit to DB
+  mysql.connection.commit()
+
+  # Close connection
+  cur.close()
+
+  return redirect(url_for('results'))
+
+
+
+@app.route('/delete_upload/<string:filename>', methods = ['GET', 'POST'])
+@is_logged_in
+def delete_upload(filename):
+  AzureStorageAccount = 'effiles'
+  key = 'axLykwdLsUwKTDY5flU6ivGrt9obV38k2UMVDCSpLYE3K6jAkwsjWOThQydhuMSWHfx6lTq102gdkas/GyKhEA=='
+  up_path = 'uploads'
+  path1 = 'efficientfrontier'
+  file_service = FileService(account_name = AzureStorageAccount, account_key = key)
+  file_service.delete_file (path1, up_path, filename)
+  # Create Cursor
+  cur = mysql.connection.cursor()
+
+  # Execute
+  cur.execute ('DELETE FROM files_uploaded WHERE filename = %s', [filename])
+
+  # Commit to DB
+  mysql.connection.commit()
+
+  # Close connection
+  cur.close()
+
+  flash ('File Deleted', 'success')
+    
+  return redirect(url_for('files'))
+
+
+
+
+@app.route('/results', methods = ['GET', 'POST'])
+@is_logged_in
+def results():
+  # Create Cursor
+  cur = mysql.connection.cursor()
+
+  #Get files
+  result = cur.execute('SELECT filename, USER, createdon FROM result_files')
+
+  files = cur.fetchall()
+
+  if result > 0:
+    return render_template('results.html', files = files)
+
+  else:
+    msg = 'No result files found, please optimize your uploaded files first.'
+    return render_template('results.html', error = msg)
+
+  # Close connection
+  cur.close()
+
+
+
+
+
+@app.route('/download_result/<string:filename>', methods = ['GET', 'POST'])
+@is_logged_in
+def download_result(filename):
+  AzureStorageAccount = 'effiles'
+  key = 'axLykwdLsUwKTDY5flU6ivGrt9obV38k2UMVDCSpLYE3K6jAkwsjWOThQydhuMSWHfx6lTq102gdkas/GyKhEA=='
+  down_path = 'results'
+  path1 = 'efficientfrontier'
+  file_service = FileService(account_name = AzureStorageAccount, account_key = key)
+  target = os.path.join(APP_ROOT, 'results/')
+  destination = '/'.join([target, filename])
+  file_service.get_file_to_path (path1, down_path, filename, destination)
+
+  
+  return send_file(destination, attachment_filename = filename)
+  
+
+
+
+
+@app.route('/delete_result/<string:filename>', methods = ['GET', 'POST'])
+@is_logged_in
+def delete_result(filename):
+  AzureStorageAccount = 'effiles'
+  key = 'axLykwdLsUwKTDY5flU6ivGrt9obV38k2UMVDCSpLYE3K6jAkwsjWOThQydhuMSWHfx6lTq102gdkas/GyKhEA=='
+  down_path = 'results'
+  path1 = 'efficientfrontier'
+  file_service = FileService(account_name = AzureStorageAccount, account_key = key)
+  file_service.delete_file (path1, down_path, filename)
+  # Create Cursor
+  cur = mysql.connection.cursor()
+
+  # Execute
+  cur.execute ('DELETE FROM result_files WHERE filename = %s', [filename])
+
+  # Commit to DB
+  mysql.connection.commit()
+
+  # Close connection
+  cur.close()
+
+  target = os.path.join(APP_ROOT, 'results/')
+  destination = '/'.join([target, filename])
+  
+  if os.path.exists(destination):
+    os.remove(destination)
+
+
+  flash ('File Deleted', 'success')
+    
+  return redirect(url_for('results'))
+
+
+
+
+if __name__ == '__main__':
+  app.secret_key = 'secret123'
+  app.run(debug = True)
---
+++ optimizer.py
@@ -0,0 +1,202 @@
+# Import dependencies
+import pandas as pd
+import numpy as np
+import pulp as pl
+import matplotlib.pyplot as plt
+import math
+import os
+import datetime
+from azure.storage.file import FileService
+
+
+
+# Define global variables
+AzureStorageAccount = 'effiles'    # Specify Azure storage account name
+key = 'axLykwdLsUwKTDY5flU6ivGrt9obV38k2UMVDCSpLYE3K6jAkwsjWOThQydhuMSWHfx6lTq102gdkas/GyKhEA=='    # 
+
+file_service = FileService(account_name = AzureStorageAccount, account_key = key) 
+path1 = 'efficientfrontier'
+up_path = 'uploads'
+down_path = 'results'
+
+
+
+# Create Azure file service connection
+# Get the user uploaded file from Azure File Service and read data
+def get_file(filename):    
+    
+
+    file_service.get_file_to_path (path1, up_path, filename, filename)
+
+    # Read data
+    df = pd.read_excel(filename, 'data')
+    df = pd.DataFrame(df)
+    
+    return df
+
+
+
+# Extract Variables
+def get_variables(filename): 
+    df = get_file(filename)
+    cost_max = math.ceil (sum (df['Cost']))
+    cost_min = int (min (df['Cost']))
+    step = int ((cost_max - cost_min)/100)
+    budget_list = list(range (cost_min, cost_max, step))
+    budget_list.append(cost_max)
+
+    return df, budget_list
+
+
+
+
+# Create the optimization function
+def optfunc (df, budget):
+    # Define the lp problem
+    prob = pl.LpProblem('ProjectSelection', pl.LpMaximize)
+    # Create the weights
+    weights=[]
+    for rownum, row in df.iterrows():
+        weightstr = str ('w' + str(rownum))  #Create naming of variables
+        weight = pl.LpVariable (str(weightstr), lowBound = 0, upBound = 1, cat = 'Integer')
+        weights.append(weight)
+    
+    # Create the optimization function
+    total_profit = ""
+    for rownum, row in df.iterrows():
+        for i, w in enumerate(weights):
+            if rownum == i:
+                total_profit += row['Benefit'] * w
+    prob += total_profit
+    
+    # Create constrains
+    total_cost = ""
+    for rownum, row in df.iterrows():
+        for i, s in enumerate(weights):
+            if rownum == i:
+                total_cost += row['Cost'] * s
+    prob += (total_cost <= budget)
+    
+    # Run optimization
+    opt_res = prob.solve()
+    sum_profit = pl.value(prob.objective)
+    
+    # Create a dictionary for v.varValue = 1
+    d = {int(v.name[1:]) : v.varValue for v in prob.variables() if v.varValue > 0}
+    key_list = list (d.keys())
+    value_list = list(d.values())
+    
+    # Calculate actual total_cost
+    ind_cost = []
+    for n in key_list:
+        c = df.loc[n, 'Cost']
+        ind_cost.append(c)
+    
+    # Define the returns
+    sum_profit = round(pl.value(prob.objective), 1)
+    #sum_cost_1 = sum([i*j for i, j in zip(value_list, ind_cost)])
+    #sum_cost_1 = round (float(sum_cost_1), 1)
+    
+    sum_cost_2 = float(np.dot(value_list, ind_cost))
+    sum_cost_2 = round (sum_cost_2, 1)
+    p_list = []
+    for pc in key_list:
+        p = df[df.index == pc]['Name']
+        name = p.base[0][0]
+        p_list.append(name)
+    
+    return [budget, sum_cost_2, sum_profit, p_list]
+
+#optfunc(df,   1207394.00)
+
+
+
+
+
+# Convert the set of results for different budgets into dataframe and dictionary, then write an output file and upload it to Azure
+def get_result(filename, plot = False):    
+    df, budget_list = get_variables(filename)
+    res_list = []
+    for i in budget_list:
+        ind_res = optfunc(df, i)
+        res_list.append(ind_res)
+    res_df = pd.DataFrame(res_list, columns = ['Budget', 'Cost', 'Benefit', 'Included:'])
+    
+    res_dict = res_df.to_dict('split')
+
+
+    if plot:
+        plt.scatter(res_df['Budget'], res_df['Benefit'])
+        plt.show()
+
+
+    # Write the result to xlsx and create scatter chart
+    out_file_name = 'Result_' + filename
+    df_e = pd.DataFrame()
+    x_axis = res_df['Budget']
+    y_axis = res_df['Benefit']
+    writer = pd.ExcelWriter (out_file_name, engine = 'xlsxwriter')
+    res_df.to_excel(writer, 'Data')
+    df_e.to_excel(writer, 'Chart')
+
+    # Access the XlsxWriter workbook and worksheet objects from the dataframe
+    workbook = writer.book
+    worksheet1 = writer.sheets['Data']
+    worksheet2 = writer.sheets['Chart']
+
+    # Add cell formats
+
+    format1 = workbook.add_format({'num_format': '$###,###,###,###,##0.0'})
+    worksheet1.set_column ('B:D', 18, format1)
+
+    # Create series for charts
+    series1 = '=Data!$B$1:$B$' + str(len(x_axis) + 1)
+    series2 = '=Data!$D$1:$D$' + str(len(y_axis) + 1)
+
+    # Create a chart object
+    chart1 = workbook.add_chart({'type': 'scatter'})
+
+    # Configure the series of the chart from the data
+    chart1.add_series({'categories': series1,
+        'values': series2,
+        'name' : 'Portfolio'})
+
+    # Format the chart
+    chart1.set_x_axis({'name': 'Budget Levels (in Millions)',
+                    'num_format': '$0',
+                    'major_unit': 5
+                    })
+    chart1.set_y_axis({'name': 'Portfolio Benefits (in Millions)',
+                    'num_format': '$0.0',
+                    'display_units': 'millions',
+                    'display_units_visible': False,
+                    'major_unit': 5000000
+                    })
+    chart1.set_title ({'name': 'Efficient Frontier'})
+    chart1.set_style(14)
+    chart1.set_size ({'width': 1200, 'height': 750})
+
+    # Insert the chart
+    worksheet2.insert_chart('A1', chart1, {'x_oofset': 25, 'y_offset': 10})
+
+    # Close
+    workbook.close()
+    writer.save()
+
+
+    # Upload the output file to Azure File Service
+    file_service.create_file_from_path(
+    path1, down_path, out_file_name, out_file_name)
+ 
+    # out_file_path = path1 + '/' + path2 + '/' + out_file_name
+
+    # Delete the files from local
+    os.remove(filename)
+    os.remove(out_file_name)
+
+
+
+    return res_dict, AzureStorageAccount, path1, down_path, out_file_name, key
+
+
+
